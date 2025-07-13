76b86f4069e2c492d6ae8484ba2ad5cd01ca0b48
Eyoab Tessera <eyoabefrem9@gmail.com>
2025-07-13 11:06:48 +0300

    python context async peration 0x02

--- python-context-async-perations-0x02/0-databaseconnection.py
+++ python-context-async-perations-0x02/0-databaseconnection.py
@@ -1,17 +1,20 @@
-#!/usr/bin/env python3
 import sqlite3
 
 class DatabaseConnection:
-    """Context manager for handling database connections."""
+    def __init__(self):
+        self.conn = None
+        self.cursor = None
+        
     def __enter__(self):
-        self.conn = sqlite3.connect('users.db')
-        return self.conn
+        self.conn = sqlite3.connect("users.db")
+        self.cursor = self.conn.cursor()
+        return self.cursor
 
     def __exit__(self, exc_type, exc_value, traceback):
         self.conn.close()
 
-with DatabaseConnection() as conn:
-    cursor = conn.cursor()
+# Usage
+with DatabaseConnection() as cursor:
     cursor.execute("SELECT * FROM users")
     results = cursor.fetchall()
     print(results)
