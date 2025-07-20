904764756ea456d2105e23fe91493a8cf9561235
Eyoab Tessera <eyoabefrem9@gmail.com>
2025-07-20 16:26:38 +0300

    0x03-Unittests_and_integration_tests

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -64,6 +64,7 @@ class TestGithubOrgClient(unittest.TestCase):
         client = GithubOrgClient("google")
         self.assertEqual(client.has_license(repo, license_key), expected)
 
+
 @parameterized_class(
     ("org_payload", "repos_payload", "expected_repos", "apache2_repos"),
     [fixtures.TEST_PAYLOAD[0]]
