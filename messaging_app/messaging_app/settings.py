afd00505d93d9a3aed886e9b73c5d8995dae57a8
Eyoab Tessera <eyoabefrem9@gmail.com>
2025-07-27 01:56:45 +0300

    Authentication and Permissions

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -54,9 +54,15 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
-    'DEFAULT_FILTER_BACKENDS': [
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_FILTER_BACKENDS': (
         'django_filters.rest_framework.DjangoFilterBackend',
-    ],
+    ),
     'DEFAULT_PAGINATION_CLASS': 'chats.pagination.MessagePagination',
     'PAGE_SIZE': 20,
 }
